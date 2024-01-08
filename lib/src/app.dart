9228bbf227004062b1efd321e0dbe0bca9be87b1
Bohaniqa <bohaniqa@gmail.com>
2024-01-08 11:31:39 +0300

    Enable Phantom

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -67,7 +67,7 @@ class _BOQAppState extends State<BOQApp> {
 
   Future<void> _connectWallet() async {
     final provider = SolanaWalletProvider.of(context);
-    provider.connect(context, options: [provider.adapter.store.apps[1]]).ignore();
+    provider.connect(context).ignore();
   }
 
   Widget _signInPopup() => ColoredBox(
